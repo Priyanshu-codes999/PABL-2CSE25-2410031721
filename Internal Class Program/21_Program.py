f99a06f0cad147040359bafde1cf7e832ceb6a75
Priyanshu-codes999 <priyanshu9999agrawal@gmail.com>
2026-02-13 10:17:48 +0530

    update

---
+++ Internal Class Program/21_Program.py
@@ -0,0 +1,16 @@
+def mindiff_chocolates(arr,m):
+    n=len(arr)
+    if m==0 or n==0:
+        return 0
+    if m>n:
+        return 0
+    arr.sort()
+    mindiff=float('inf')
+    for i in range(n-m+1):
+        diff=arr[i+m-1]-arr[i]
+        mindiff=min(mindiff,diff)
+    return mindiff
+
+arr=[3,4,1,9,56,7,9,12]
+m=5
+print(f"output: {mindiff_chocolates(arr,m)}")
